0fc86d5e25227769e7d78d0665e09fb8a43721b8
Sergei Bogko <sergeibogko85@gmail.com>
2021-11-07 23:10:41 +0200

    adaptation finish

--- app/js/main.js
+++ app/js/main.js
@@ -10,6 +10,10 @@ $(function () {
     $(".menu").toggleClass("menu--open");
   });
 
+  if ($(window).width() < 651) {
+    $(".works-path__item--measuring").appendTo($(".works-path__items-box"));
+  }
+
   $(".top__slider").slick({
     dots: true,
     arrows: false,
@@ -22,6 +26,50 @@ $(function () {
     slidesToScroll: 10,
     dots: true,
     arrows: false,
+    responsive: [
+      {
+        breakpoint: 1700,
+        settings: {
+          slidesToShow: 8,
+          slidesToScroll: 8,
+        },
+      },
+      {
+        breakpoint: 1511,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 6,
+        },
+      },
+      {
+        breakpoint: 1201,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+        },
+      },
+      {
+        breakpoint: 841,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 551,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 376,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
 
   $(".article-slider__box").slick({
